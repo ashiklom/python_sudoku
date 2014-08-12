a8cc8f7fe89206b75facb8afa4f6db1f5d62677b
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2014-08-12 12:36:16 -0400

    Condensed 'oneplace' method via nested function

--- sudoku_methods.py
+++ sudoku_methods.py
@@ -39,7 +39,6 @@ def fill_onepos(grid):
             grid[ind[0]][ind[1]] = val[0]
             fill_counter += 1
             length_counter = len(possibles)
-#    pp(all_possibles(grid))
     if fill_counter == 0 and length_counter > 0:
         raise FailedFirstPass("No single-possibility cells remaining. Try another method.")
     return grid
@@ -59,7 +58,6 @@ def fill_onlyplace(grid):
         FailedSecondPass: If no cells can be filled via this method.
     """
 
-
     def unique_val(numlist, possibles):
         """Scans a Sudoku row, column, or box for missing values, \
         identifies if there is only one place missing values can appear, \
@@ -72,9 +70,11 @@ def fill_onlyplace(grid):
                 list comprehension with 'get_possibles' function
 
         Returns:
-            Tuple of unique value index, its value.
+            List of tuples of unique value index, its value; or 'None' if no \
+                unique values found.
         """
         missing = get_missing(numlist)
+        unique_vals = []
         for m in missing:
             present_counter = 0
             for ind, val in enumerate(possibles):
@@ -82,58 +82,44 @@ def fill_onlyplace(grid):
                     present_counter += 1
                     return_ind = ind
                 if present_counter == 1:
-                    return return_ind, m
-                else:
-                    return None
+                    unique_vals.append((return_ind, m))
+        if len(unique_vals) > 0:
+            return unique_vals
+        else:
+            return None
 
     fill_counter = 0
     while fill_counter == 0:
         for row_num, row in enumerate(grid):
-            missing = get_missing(row)
             blanks = find_blanks(row)
             possibles_in_row = [get_possibles(grid, row_num, col_num) for col_num in blanks]
-            for m in missing:
-                present_counter = 0
-                for ind, val in enumerate(possibles_in_row):
-                    if m in val:
-                        present_counter += 1
-                        col_num = ind
-                if present_counter == 1:
-                    grid[row_num][col_num] = m
+            unique = unique_val(row, possibles_in_row)
+            if unique is not None:
+                for col_num, value in unique:
+                    grid[row_num][col_num] = value
                     fill_counter += 1
 
         for col_num in range(9):
             column = get_column(grid, col_num)
-            missing = get_missing(column)
             blanks = find_blanks(column)
             possibles_in_column = [get_possibles(grid, row_num, col_num) for row_num in blanks]
-            for m in missing:
-                present_counter = 0
-                for ind, val in enumerate(possibles_in_column):
-                    if m in val:
-                        present_counter += 1
-                        row_num = ind
-                if present_counter == 1:
-                    grid[row_num][col_num] = m
+            unique = unique_val(column, possibles_in_column)
+            if unique is not None:
+                for row_num, value in unique:
+                    grid[row_num][col_num] = value
                     fill_counter += 1
 
         for box_row in range(0, 9, 3):
             for box_column in range(0, 9, 3):
                 box = get_box(grid, box_row, box_column)
-                missing = get_missing(box)
                 blanks = find_blanks(box)
                 blanks_index = [box_ref(ind, box_row, box_column) for ind in blanks]
                 possibles_in_box = [get_possibles(grid, row_num, col_num)
                                     for (row_num, col_num) in blanks_index]
-                for m in missing:
-                    present_counter = 0
-                    for ind, val in enumerate(possibles_in_box):
-                        if m in val:
-                            present_counter += 1
-                            box_num = ind
-                        if present_counter == 1:
-                            row_num, col_num = box_ref(box_num, box_row, box_column)
-                            fill_counter += 1
-
-
+                unique = unique_val(box, possibles_in_box)
+                if unique is not None:
+                    for box_num, value in unique:
+                        row_num, col_num = box_ref(box_num, box_row, box_column)
+                        grid[row_num][col_num] = value
+                        fill_counter += 1
 
